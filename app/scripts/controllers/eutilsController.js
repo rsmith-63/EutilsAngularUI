4ca22efa8f28fb94d61187b52f4ddf36ac79a0d8
Rob Smith <rsmith_63@yahoo.com>
2017-02-20 09:45:12 -0500

    initial commit

---
+++ app/scripts/controllers/eutilsController.js
@@ -0,0 +1,13 @@
+/**
+ * Created by rob on 2/17/2017.
+ */
+'use strict';
+
+angular.module('eutilsUI').controller('searchController', ['$scope', 'searchNcbi', function($scope, searchNcbi) {
+    searchNcbi.getDataBases().query(function(response){
+            $scope.dataBases = response;
+        },
+        function(response){
+            $scope.message = "Error: "+response.status + " " + response.statusText;
+        });
+}])
