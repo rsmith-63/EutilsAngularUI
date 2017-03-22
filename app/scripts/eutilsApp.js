ed7e7c094cc77b6b20682d778f71407742e7df16
Rob Smith <rsmith_63@yahoo.com>
2017-03-22 15:42:55 -0400

    Move routes to separate file routes

--- app/scripts/eutilsApp.js
+++ app/scripts/eutilsApp.js
@@ -4,67 +4,6 @@
 'use strict';
 angular.module('eutilsUI', ['ui.router', 'ngResource' ,'LocalStorageModule'])
     .constant("baseURL","http://localhost:4000/api")
-    // .config(function($stateProvider, $urlRouterProvider) {
-    //     $stateProvider
-    //     // route for the home page
-    //         .state('app', {
-    //             url: '/',
-    //             views: {
-    //
-    //                 'header': {
-    //                     //templateUrl: 'views/main.html',
-    //                     component: 'testComp',
-    //                     resolve: {
-    //
-    //                         dataBases: ["NcbiResources", function (NcbiResources) {
-    //                             return NcbiResources.getDbList().$promise.catch(() => []);
-    //                         }]
-    //                     },
-    //                     bindings: {
-    //                         dataBases: 'dataBases'
-    //                     }
-    //
-    //                 }
-    //
-    //             }
-    //         })
-    //         .state('app.results', {
-    //             url:'results',
-    //             views: {
-    //                 'content@': {
-    //                     templateUrl : 'views/Results.html',
-    //                     controller  : 'resultsController',
-    //                     resolve: {
-    //
-    //                         searchResults: ["localStorageService", function (localStorageService) {
-    //                             return localStorageService.get('searchResults')
-    //                         }]
-    //                     }
-    //                 }
-    //             }
-    //         })
-    //         .state('app.listDetails', {
-    //             url:'^listDetails',
-    //             params: {
-    //                 term: null,
-    //                 db:null
-    //             },
-    //             views: {
-    //                 'content@': {
-    //                     templateUrl : 'views/info.html',
-    //                     controller  : 'detailsController',
-    //                     resolve: {
-    //
-    //                         detailResults: ["$stateParams", function ($stateParams) {
-    //                             return $stateParams;
-    //                         }]
-    //                     }
-    //                 }
-    //             }
-    //         });
-    //
-    //     $urlRouterProvider.otherwise('/');
-    // })
     .config(function(localStorageServiceProvider){
         localStorageServiceProvider.setPrefix('eutilsUI')
             .setNotify(false, false);
